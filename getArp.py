4a9e42a482d4adcc9d446e05633b863e728b1558
Scott M Gardner <sgardne@gmail.com>
2016-04-10 20:31:55 -0500

    some preliminary work.

--- getArp.py
+++ getArp.py
@@ -8,6 +8,17 @@ import netmiko, sqlite3
 from sys import exit
 from getpass import getpass
 from os import path
+from hashlib import sha1
+
+class Indexer:
+    """Create a unique index for each entry"""
+    def __init__(self, encoded_string):
+        self.hasher = sha1()
+        self.hasher.update(encoded_string)
+        self.digest = self.hasher.hexdigest()
+        
+    def __str__(self):
+        return self.digest
 
 def main():
 
@@ -19,8 +30,12 @@ def main():
     else:
         # Create a new database
         create_db = input("Database does not exist. Create it now? (y/n): ")
-        print(create_db)
-        exit()
+        if create_db.lower() == "y":
+            con = sqlite3.connect("arp.db")
+            con.execute('create table arp_entry(indx, IP, MAC, firstSeen,\
+                         lastSeen, router)')
+        else:
+            exit()
 
     # Receive a list of IP addresses to get ARP info from.
 
@@ -29,7 +44,7 @@ def main():
     # Add new entry or update existing
     ## Entries: indx, IP, MAC, firstSeen, lastSeen, router
     ## Figure out a way to make indx unique per entry. Could have same ip
-    ## multiple routers. Maybe hashing of some kind with ip/firstSeen/router
+    ## multiple routers. Maybe hashing of some kind with IP/MAC/router
     exit()
 
 if __name__ == "__main__":
