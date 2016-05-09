d842a72fae102d18d60558269c60fd7ef9b3f9f0
ScottG <sgardne@gmail.com>
2016-05-08 19:10:38 -0500

    works pretty well now. need to iron out some bugs still

--- IP_LIST.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-IP_LIST = ['10.0.0.1', ]
--- getArp.py
+++ getArp.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 """
-Track ARP entries over time. Requires netmiko - 
-https://github.com/ktbyers/netmiko
+Track ARP entries over time.
 """
 
 import netmiko, sqlite3, hashlib, sys, os
 import csv
 from getpass import getpass
-from IP_LIST import IP_LIST
+from DEVICE_LIST import DEVICES
+from datetime import datetime
+
+def GetCreds():
+    """Return the username and password as a list."""
+
+    _username = input("Router username: ")
+    _password = getpass("Password for {}: ".format(_username))
+    return [_username, _password]
 
 class Indexer:
     """Create a unique index for each entry"""
@@ -22,47 +29,54 @@ class Indexer:
 class Arper:
     def __init__(self, router_dict):
         self.router_dict = router_dict
-        self.ssh = netmiko.ConnectHandler(**router_dict)
-        self.arp_table = self.ssh.send_command_expect('show arp no-resolve')
+        try:
+            self.ssh = netmiko.ConnectHandler(**self.router_dict)
+        except netmiko.ssh_exception.NetMikoAuthenticationException:
+            raise
+        except Exception as e:
+            raise
+
+        if self.router_dict['device_type'] == 'juniper':
+            self.arp_table = self.ssh.send_command_expect('show arp no-resolve')
+        else:
+            print("{} is not Juniper.".format(self.router_dict['device_type']))
 
     def __str__(self):
         return self.arp_table
 
 def main():
     arp_db = "arp.db"
-
-    username = input("Router username: ")
-    password = getpass("Password for {}: ".format(username))
-
-    JUNIPER_SWITCH = {
-        'device_type':          'juniper',
-        'ip':                   '',
-        'username':             username,
-        'password':             password,
-        'verbose':              False,
-    }
+    username, password = GetCreds()
 
     # Check for and set up the database
     if not os.path.isfile(arp_db):
         # Create a new database
         create_db = input("Database does not exist. Create it now? (y/n): ")
+
         if create_db.lower() == "y":
-            con = sqlite3.connect("arp.db")
-            con.execute('create table arp_entry(indx, IP, MAC, firstSeen,\
-                         lastSeen, router)')
+            con = sqlite3.connect(arp_db)
+            cur = con.cursor()
+            cur.execute("create table arp_entry(indx, IP, MAC, firstSeen, lastSeen, router)")
+            cur.execute('create unique index entries on arp_entry (indx)')
+            con.commit()
+            con.close()
         else:
             sys.exit()
 
     # Receive a list of router IP addresses to get ARP info from.
-    ## Imported from IP_LIST.py
-    for router_ip in IP_LIST:
+    con = sqlite3.connect(arp_db)
+    cur = con.cursor()
+
+    now_datetime = datetime.now()
+    now_timestamp = now_datetime.timestamp()
+    for device in DEVICES:
         # Get the arp table
-        JUNIPER_SWITCH['ip'] = router_ip
-        JUNIPER_SWITCH['username'] = username
-        JUNIPER_SWITCH['password'] = password
-        arp_table = Arper(JUNIPER_SWITCH)
+        device['username'] = username
+        device['password'] = password
+        arp_table = Arper(device)
 
         # parse the table as csv
+        ## Create helper function here checking for device type.
         csvreader = csv.reader(arp_table.arp_table.splitlines(), delimiter='\t')
         for row in csvreader:
             if not row:
@@ -70,22 +84,23 @@ def main():
             splitrow = row[0].rsplit()
             mac = splitrow[0]
             ip = splitrow[1]
-            if "Total" in mac:
+            if "Total" in mac or "MAC" in mac:
                 continue
             else:
-                string_to_encode = mac + ip + router_ip
+                string_to_encode = mac + ip + device['ip']
                 encoded_str = string_to_encode.encode()
                 indxr = Indexer(encoded_str)
 
-            # Check database for existing entry
-
-            
-
-        # Add new entry or update existing
-        ## Entries: indx, IP, MAC, firstSeen, lastSeen, router
-        ## Figure out a way to make indx unique per entry. Could have same ip
-        ## multiple routers. Maybe hashing of some kind with IP/MAC/router
-
+            # Try to add new entry
+            try:
+                cur.execute("INSERT INTO arp_entry VALUES (?,?,?,?,?,?)", [str(indxr), ip, mac, now_timestamp, now_timestamp, device['ip']])
+                con.commit()
+            except sqlite3.IntegrityError:
+                # Update the lastSeen value.
+                cur.execute("UPDATE arp_entry SET lastSeen = ? WHERE indx = ?", [now_timestamp, str(indxr)])
+                con.commit()
+            except Exception as e:
+                raise
     sys.exit()
 
 if __name__ == "__main__":
